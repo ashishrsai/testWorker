19d60f8a85c78a5ff0e270653eed904ccedc1ffc
ashishrsai <ashishkumarsai@me.com>
2020-06-19 18:11:53 +0100

    build

---
+++ client/build/precache-manifest.e9e0660733e21a39773810a1bbff89c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87443e67d027d24be6bc472b763f55f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27c7d1dccf5fd8632d7",
+    "url": "/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "31ee20f4cbf9998be6d1",
+    "url": "/static/css/main.6c917b14.chunk.css"
+  },
+  {
+    "revision": "a27c7d1dccf5fd8632d7",
+    "url": "/static/js/2.4e0984d8.chunk.js"
+  },
+  {
+    "revision": "98522e9b600fb3c50caeda85927f9bc2",
+    "url": "/static/js/2.4e0984d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ee20f4cbf9998be6d1",
+    "url": "/static/js/main.5755ab13.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
